e92c00bb3580446d8b940a3c1312849d6a2af10b
Diana R <romanuk13082006@gmail.com>
2024-05-26 19:50:44 +0300

    Add project files.

---
+++ Lab12/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿using System.Windows.Forms;
+
+namespace Lab12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RepeatBubbleInt = new System.Windows.Forms.Button();
+            this.PartlySortedBubbleInt = new System.Windows.Forms.Button();
+            this.ChaotBubbleInt = new System.Windows.Forms.Button();
+            this.PartlySortedQuickInt = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RepeatBubbleInt
+            // 
+            this.RepeatBubbleInt.BackColor = System.Drawing.Color.LemonChiffon;
+            this.RepeatBubbleInt.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.RepeatBubbleInt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.RepeatBubbleInt.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.RepeatBubbleInt.Location = new System.Drawing.Point(60, 264);
+            this.RepeatBubbleInt.Name = "RepeatBubbleInt";
+            this.RepeatBubbleInt.Size = new System.Drawing.Size(199, 55);
+            this.RepeatBubbleInt.TabIndex = 12;
+            this.RepeatBubbleInt.Text = "Скарби з повторами";
+            this.RepeatBubbleInt.UseVisualStyleBackColor = false;
+            this.RepeatBubbleInt.Click += new System.EventHandler(this.Duplicates_Click);
+            // 
+            // PartlySortedBubbleInt
+            // 
+            this.PartlySortedBubbleInt.BackColor = System.Drawing.Color.LemonChiffon;
+            this.PartlySortedBubbleInt.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.PartlySortedBubbleInt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.PartlySortedBubbleInt.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.PartlySortedBubbleInt.Location = new System.Drawing.Point(60, 171);
+            this.PartlySortedBubbleInt.Name = "PartlySortedBubbleInt";
+            this.PartlySortedBubbleInt.Size = new System.Drawing.Size(199, 55);
+            this.PartlySortedBubbleInt.TabIndex = 11;
+            this.PartlySortedBubbleInt.Text = "Частково відсортовані скарби";
+            this.PartlySortedBubbleInt.UseVisualStyleBackColor = false;
+            this.PartlySortedBubbleInt.Click += new System.EventHandler(this.PartiallySorted_Click);
+            // 
+            // ChaotBubbleInt
+            // 
+            this.ChaotBubbleInt.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ChaotBubbleInt.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ChaotBubbleInt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.ChaotBubbleInt.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ChaotBubbleInt.Location = new System.Drawing.Point(60, 84);
+            this.ChaotBubbleInt.Name = "ChaotBubbleInt";
+            this.ChaotBubbleInt.Size = new System.Drawing.Size(199, 55);
+            this.ChaotBubbleInt.TabIndex = 10;
+            this.ChaotBubbleInt.Text = "Хаотичний перелік скарбів";
+            this.ChaotBubbleInt.UseVisualStyleBackColor = false;
+            this.ChaotBubbleInt.Click += new System.EventHandler(this.Chaotic_Click);
+            // 
+            // PartlySortedQuickInt
+            // 
+            this.PartlySortedQuickInt.BackColor = System.Drawing.Color.LemonChiffon;
+            this.PartlySortedQuickInt.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.PartlySortedQuickInt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.PartlySortedQuickInt.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.PartlySortedQuickInt.Location = new System.Drawing.Point(910, 785);
+            this.PartlySortedQuickInt.Name = "PartlySortedQuickInt";
+            this.PartlySortedQuickInt.Size = new System.Drawing.Size(174, 55);
+            this.PartlySortedQuickInt.TabIndex = 13;
+            this.PartlySortedQuickInt.Text = "MSD";
+            this.PartlySortedQuickInt.UseVisualStyleBackColor = false;
+            this.PartlySortedQuickInt.Click += new System.EventHandler(this.MSD_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox2.Location = new System.Drawing.Point(60, 341);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(197, 26);
+            this.textBox2.TabIndex = 16;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(779, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 25);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Список скарбів";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(12, 469);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(295, 82);
+            this.label2.TabIndex = 18;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(12, 558);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(295, 86);
+            this.label3.TabIndex = 19;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(55, 370);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(202, 99);
+            this.label4.TabIndex = 21;
+            this.label4.Text = " Загальна кількість\r\n скарбів у родинах";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(358, 72);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(1047, 695);
+            this.textBox1.TabIndex = 22;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.Location = new System.Drawing.Point(12, 644);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(295, 86);
+            this.label5.TabIndex = 23;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LemonChiffon;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(672, 785);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 55);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "LSD";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.LSD_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Snow;
+            this.ClientSize = new System.Drawing.Size(1837, 878);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.PartlySortedQuickInt);
+            this.Controls.Add(this.RepeatBubbleInt);
+            this.Controls.Add(this.PartlySortedBubbleInt);
+            this.Controls.Add(this.ChaotBubbleInt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RepeatBubbleInt;
+        private System.Windows.Forms.Button PartlySortedBubbleInt;
+        private System.Windows.Forms.Button ChaotBubbleInt;
+        private System.Windows.Forms.Button PartlySortedQuickInt;
+        private System.Windows.Forms.TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private Label label5;
+        private Button button1;
+    }
+}
+
---
+++ Lab12/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.Remoting;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lab12
+{
+    public partial class Form1 : Form
+    {
+        private int[] initialArray;
+        private int[] arrayToSort;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public static int[] Chaotic(int length)
+        {
+            Random random = new Random();
+            int[] a = new int[length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = random.Next(1, 100000);
+            }
+            return a;
+        }
+
+        public static int[] PartiallySorted(int length)
+        {
+            Random random = new Random();
+            int[] a = new int[length];
+            a[0] = random.Next(1, 300);
+            for (int i = 1; i < a.Length; i++)
+            {
+                a[i] = a[i - 1] + random.Next(1, 100000);
+            }
+
+            int[] b = Chaotic(length / 2);
+            for (int i = 0; i < (length / 2); i++)
+            {
+                a[i] = b[i];
+            }
+            return a;
+        }
+
+        public static int[] Duplicates(int length)
+        {
+            Random random = new Random();
+            int[] a = new int[length];
+            for (int i = 1; i < a.Length; i++)
+            {
+                a[i] = random.Next(1, 100000);
+                if (a[i] % 2 == 0)
+                {
+                    a[i] = a[i - 1];
+                }
+            }
+            return a;
+        }
+
+        public void BubbleSort(int[] a)
+        {
+            int temp;
+            bool swapped;
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < a.Length - i - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static void Swap(int[] a, int i, int j)
+        {
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+
+        public static void SortByLSD(int[] a, int radix)
+        {
+            int n = a.Length;
+            int min = a[0];
+            int max = a[0];
+            
+            for (int i = 1; i < n; i++)
+            {
+                if (a[i] < min)
+                {
+                    min = a[i];
+                }
+                else if (a[i] > max)
+                {
+                    max = a[i];
+                }
+            }
+
+            int exp = 1;
+            while ((max - min) / exp >= 1)
+            {
+                CountByDigit(a, radix, exp, min);
+                exp *= radix;
+            }
+        }
+
+        public static void CountByDigit(int[] a, int radix, int exp, int min)
+        {
+            int n = a.Length;
+            int bucketIndex;
+            int[] buckets = new int[radix];
+            int[] result = new int[n];
+
+            for (int i = 0; i < radix; i++)
+            {
+                buckets[i] = 0;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                bucketIndex = ((a[i] - min) / exp) % radix;
+                buckets[bucketIndex]++;
+            }
+
+            for (int i = 1; i < radix; i++)
+            {
+                buckets[i] += buckets[i - 1];
+            }
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                bucketIndex = ((a[i] - min) / exp) % radix;
+                result[--buckets[bucketIndex]] = a[i];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = result[i];
+            }
+        }
+
+
+        public static int Partition(int[] a, int low, int high)
+        {
+            int pivot = a[high];
+            int i = low - 1;
+            for (int j = low; j <= high - 1; j++)
+            {
+                if (a[j] < pivot)
+                {
+                    i++;
+                    Swap(a, i, j);
+                }
+            }
+            Swap(a, i + 1, high);
+            return i + 1;
+        }
+
+        public void SortByMSD(int[] a, int low, int high, int digit)
+        {
+            if (high <= low || digit < 0)
+            {
+                return;
+            }
+
+            int[] count = new int[12];
+            int[] temp = new int[high - low + 1];
+
+            for (int i = low; i <= high; i++)
+            {
+                int character = (int)(a[i] / Math.Pow(10, digit - 1)) % 10;
+                count[character + 2]++;
+            }
+
+            for (int j = 0; j < 11; j++)
+            {
+                count[j + 1] += count[j];
+            }
+
+            for (int i = low; i <= high; i++)
+            {
+                int character = (int)(a[i] / Math.Pow(10, digit - 1)) % 10;
+                temp[count[character + 1]++] = a[i];
+            }
+
+            for (int i = low; i <= high; i++)
+            {
+                a[i] = temp[i - low];
+            }
+
+            for (int j = 0; j < 10; j++)
+            {
+                SortByMSD(a, low + count[j], low + count[j + 1] - 1, digit - 1);
+            }
+        }
+
+        public void RadixSortMSD(int[] a)
+        {
+            int max = a[0];
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] > max)
+                {
+                    max = a[i];
+                }
+            }
+
+            int digit = (int)Math.Floor(Math.Log10(Math.Abs(max))) + 1;
+            SortByMSD(a, 0, a.Length - 1, digit);
+        }
+
+       
+        public void Chaotic_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            int length;
+            if (!int.TryParse(textBox2.Text.Trim(), out length))
+            {
+                MessageBox.Show("Будь ласка, введіть коректну довжину масиву!", "Помилка", MessageBoxButtons.OK);
+                return;
+            }
+
+            initialArray = Chaotic(length);
+            arrayToSort = (int[])initialArray.Clone();
+            PrintArray(textBox1, initialArray, "Початковий масив:");
+        }
+
+        public void PartiallySorted_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            int length;
+            if (!int.TryParse(textBox2.Text.Trim(), out length))
+            {
+                MessageBox.Show("Будь ласка, введіть коректну довжину масиву!", "Помилка", MessageBoxButtons.OK);
+                return;
+            }
+
+            initialArray = PartiallySorted(length);
+            arrayToSort = (int[])initialArray.Clone();
+            PrintArray(textBox1, initialArray, "Початковий масив:");
+        }
+
+        public void Duplicates_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            int length;
+            if (!int.TryParse(textBox2.Text.Trim(), out length))
+            {
+                MessageBox.Show("Будь ласка, введіть коректну довжину масиву!", "Помилка", MessageBoxButtons.OK);
+                return;
+            }
+
+            initialArray = Duplicates(length);
+            arrayToSort = (int[])initialArray.Clone();
+            PrintArray(textBox1, initialArray, "Початковий масив:");
+        }
+
+        private void MSD_Click(object sender, EventArgs e)
+        {
+            if (initialArray == null)
+            {
+                MessageBox.Show("Спочатку згенеруйте масив!", "Помилка", MessageBoxButtons.OK);
+                return;
+            }
+
+            Stopwatch stopwatch = new Stopwatch();
+            TimeSpan timespan;
+
+            arrayToSort = (int[])initialArray.Clone();
+            textBox1.Clear();
+            PrintArray(textBox1, initialArray, "Початковий масив:");
+            textBox1.AppendText(Environment.NewLine);
+
+            stopwatch.Start();
+            RadixSortMSD(arrayToSort);
+            stopwatch.Stop();
+
+            PrintArray(textBox1, arrayToSort, "Відсортований масив (MSD RadixSort):");
+            timespan = stopwatch.Elapsed;
+            label3.Text = $"Час виконання MSD RadixSort в мс: " + (double)timespan.Ticks / 10000;
+        }
+
+        public void LSD_Click(object sender, EventArgs e)
+        {
+            if (initialArray == null)
+            {
+                MessageBox.Show("Спочатку згенеруйте масив!", "Помилка", MessageBoxButtons.OK);
+                return;
+            }
+
+            Stopwatch stopwatch = new Stopwatch();
+            TimeSpan timespan;
+
+            arrayToSort = (int[])initialArray.Clone();
+            textBox1.Clear();
+            PrintArray(textBox1, initialArray, "Початковий масив:");
+            textBox1.AppendText(Environment.NewLine);
+
+            stopwatch.Start();
+            SortByLSD(arrayToSort, 256);
+            stopwatch.Stop();
+
+            PrintArray(textBox1, arrayToSort, "Відсортований масив (LSD Radix Sort):");
+            timespan = stopwatch.Elapsed;
+            label2.Text = $"Час виконання LSD Radix Sort в мс: " + (double)timespan.Ticks / 10000;
+        }
+
+       
+
+        static void PrintArray(System.Windows.Forms.TextBox textBox1, int[] array, string label)
+        {
+            textBox1.AppendText(label + " " + string.Join(" ", array) + Environment.NewLine);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
